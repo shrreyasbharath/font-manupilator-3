677f9dff8ba74d95bf42ad00b9233e6fd4685e8a
shrreyasbharath <85285429+shrreyasbharath@users.noreply.github.com>
2024-04-10 13:19:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function draw() {
+    background('#969A97');
+    textSize(difference);
+
+    fill('#F90093');
+    stroke('#F90093');
+    text("Shrreyas", noseX, noseY);
+    document.getElementById("size").innerHTML = "size of the font " + difference + "px";
+    
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + " nose Y = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.y;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = "+ rightWristX + "difference = " + difference);
+    }
+}
